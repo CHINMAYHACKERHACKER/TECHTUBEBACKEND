35b7c33da27f7e8ca2d8ddffb86b4f458d28df64
HACKER <albertkanashetti@gmail.com>
2023-04-07 18:34:03 +0530

    BACKENDTECHCHANGE

--- vdeoplay/index.js
+++ vdeoplay/index.js
@@ -134,6 +134,7 @@ app.post("/VIDEO", VIDEO.array('VIDEO'), (req, res, next) => {
   const NOISEREDUCE = req.body.NOISEREDUCE;
   const pathToVideo = DESTINATION + VIDEO;
   const AUDIO = req.body.AUDIO;
+  const USERAUDIO = req.body.USERAUDIO;
   const targetFolder = './VIDEONOISEREDUCE';
 
   // fs.rename(req.files[1].path, `${targetFolder}/${req.files[1].filename}`, function (err) {
@@ -323,6 +324,90 @@ app.post("/VIDEO", VIDEO.array('VIDEO'), (req, res, next) => {
       });
     });
   }
+  else if (USERAUDIO == "yes") {
+
+    const targetFolder = './VIDEONOISEREDUCE';
+
+    fs.rename(req.files[0].path, `${targetFolder}/${req.files[0].filename}`, function (err) {
+      if (err) throw err;
+      console.log('File moved successfully!');
+    });
+    fs.rename(req.files[1].path, `${targetFolder}/${req.files[1].filename}`, function (err) {
+      if (err) throw err;
+      console.log('File moved successfully!');
+    });
+
+    const INPUTFILEPATH = DESTINATION + VIDEO;
+    const FILEPATH = `VIDEONOISEREDUCE/${req.files[1].filename}`;
+    const OUTPUTFILEPATH = `VIDEONOISEREDUCE/${VIDEO}`;
+
+    COMAND = `ffmpeg -y -i ${OUTPUTFILEPATH} -i ${FILEPATH} -filter_complex "[1:a]volume=0.5[a1];[0:a]equalizer=f=1000:width_type=h:width=100:g=-50,volume=15.0[a];[a][a1]amix=inputs=2" ${OUTPUTFILEPATH + "temp.mp4"} && mv ${OUTPUTFILEPATH + "temp.mp4"} ${OUTPUTFILEPATH}`;
+    exec(COMAND, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("BACKGROUND MUSIC ADDED TO VIDEO");
+
+      const FILEONE = `${OUTPUTFILEPATH}` + "_" + "1080p" + ".mp4";
+      const outputFile1080p = FILEONE.replace(/\.mp4_/, '_');
+
+      const FILETWO = `${OUTPUTFILEPATH}` + "_" + "720p" + ".mp4";
+      const outputFile720p = FILETWO.replace(/\.mp4_/, '_');
+
+      const FILETHREE = `${OUTPUTFILEPATH}` + "_" + "540p" + ".mp4";
+      const outputFile540p = FILETHREE.replace(/\.mp4_/, '_');
+
+      const FILEFIVE = `${OUTPUTFILEPATH}` + "_" + "360p" + ".mp4";
+      const outputFile360p = FILEFIVE.replace(/\.mp4_/, '_');
+
+      // Create a new command using fluent-ffmpeg
+      const command = ffmpeg();
+
+      // Set input stream
+      command.input(`${OUTPUTFILEPATH}`);
+
+      // Set video codec to libx264 to maintain quality
+      command.videoCodec('libx264');
+
+      // Set a lower bitrate to reduce file size
+      command.videoBitrate('800k');
+
+      // Set audio codec to aac
+      command.audioCodec('aac');
+
+      // Set a lower audio bitrate to reduce file size
+      command.audioBitrate('128k');
+
+      // Set output file paths for each resolution
+      command.output(outputFile1080p)
+        .videoFilters('scale=w=1920:h=1080')
+        .outputOptions('-c:a copy');
+      command.output(outputFile720p)
+        .videoFilters('scale=w=1280:h=720')
+        .outputOptions('-c:a copy');
+      command.output(outputFile540p)
+        .videoFilters('scale=w=960:h=540')
+        .outputOptions('-c:a copy');
+      command.output(outputFile360p)
+        .videoFilters('scale=w=640:h=360')
+        .outputOptions('-c:a copy');
+
+      // Run the command and log the output
+      command.on('error', (err) => {
+        console.error('An error occurred:', err.message);
+      }).on('end', () => {
+        console.log('Compression complete!');
+        const INSERT_QUERY = `INSERT INTO USERVIDEOLIST (TITLE, USERID, VIDEOONE, VIDEOTWO, VIDEOTHREE, VIDEOFIVE, VIDEONOISEREDUCE, VIDEOMUSIC, USERAUDIO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        const values = [TITLE, USERUNIQUEID, outputFile1080p, outputFile720p, outputFile540p, outputFile360p, NOISEREDUCE, AUDIO, USERAUDIO];
+        con.query(INSERT_QUERY, values, (err, result) => {
+          if (err) throw err;
+          console.log("Video inserted into database");
+          res.send("Video uploaded and compressed successfully");
+        });
+      }).run();
+    });
+  }
   else {
 
     // Define output file paths for each resolution
@@ -385,9 +470,8 @@ app.post("/VIDEO", VIDEO.array('VIDEO'), (req, res, next) => {
   setTimeout(() => {
     METHOD(VIDEO, songName)
   }, 60000)
+
 });
-
-
 
 app.get("/USERVIDEOVIDEO", VIDEO.single('VIDEO'), (req, res, next) => {
   con.query(`SELECT * FROM USERVIDEOLIST`, (ERR, DATA, fields) => {
